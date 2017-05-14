3e2bd544936dd6da1d07fff48db26d9efa860d77
Alton-Y <alton.yeung@ryerson.ca>
2017-05-14 00:19:04 -0400

    complete model

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -3,19 +3,19 @@ import cv2
 import numpy as np
 
 lines = []
-with open('/Users/altonyeung/Google Drive/Udacity/Git/self-driving-car/CarND-Behavioral-Cloning-P3/simdata/rev1/driving_log.csv') as csvfile:
+with open('/Users/altonyeung/Google Drive/Udacity/Git/self-driving-car/CarND-Behavioral-Cloning-P3/simdata/training_2/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
 images = []
 measurements = []
-correction = 0.25
+correction = 0.20
 for line in lines: #loop through each line in the csv
     for i in range(3):
         source_path = line[i] #get file name of center image
         filename = source_path.split('\\')[-1] #extract only the file name from file path
-        current_path = '/Users/altonyeung/Google Drive/Udacity/Git/self-driving-car/CarND-Behavioral-Cloning-P3/simdata/rev1/IMG/' + filename
+        current_path = '/Users/altonyeung/Google Drive/Udacity/Git/self-driving-car/CarND-Behavioral-Cloning-P3/simdata/training_2/IMG/' + filename
         image = cv2.imread(current_path)
         images.append(image)
         measurement = float(line[3]) # get steering angle 
@@ -37,7 +37,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Activation, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -51,12 +51,22 @@ model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10))
+model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
+
+# model.load_weights('model_weights.h5')
+
+
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save('model_nvdax3.h5')
+model.save_weights('model_weights.h5')
+model.save('model_nvdax_x4.h5')
 
